dcc21b89398b6aa38e96e952b5e0bdfb81d2d0b1
GiuliZuppo <139927146+GiuliZuppo@users.noreply.github.com>
2023-09-04 17:53:48 -0300

    Add files via upload

--- stock_data_dag.py
+++ stock_data_dag.py
@@ -5,6 +5,9 @@ from datetime import datetime, timedelta
 import requests
 import pandas as pd
 import psycopg2
+import smtplib
+from airflow.operators.email_operator import EmailOperator
+
 
 # Configurar los argumentos por defecto para el DAG
 default_args = {
@@ -21,6 +24,18 @@ dag = DAG(
     default_args=default_args,
     schedule_interval=timedelta(days=1),
 )
+
+def verificar_condicion_alerta(df):
+    limite_alerta_volumen = 30
+    ultimo_volumen = df.iloc[-1]['volume']
+    
+    if ultimo_volumen < limite_alerta_volumen:
+        return "El volumen de la acción ha caído por debajo del límite de alerta: {0}".format(ultimo_volumen)
+    else:
+        return None  # No se envía correo electrónico si no se cumple la condición
+
+
+df = None
 
 # Definir la función para la tarea de ingestión de datos
 def ingest_data_function():
@@ -52,58 +67,72 @@ def ingest_data_function():
         time_series_data = data_json.get("Time Series (5min)", {})
         df = pd.DataFrame.from_dict(time_series_data, orient="index")
         df.index.name = 'Fecha'
+
+        #Verificar la condicion de alerta
+        mensaje_alerta = verificar_condicion_alerta(df)
+        
+        # Conectar a la base de datos Redshift
+        try:
+            connection = psycopg2.connect(
+                host=redshift_host,
+                port=redshift_port,
+                dbname=redshift_dbname,
+                user=redshift_user,
+                password=redshift_password
+            )
+            cursor = connection.cursor()
+
+            # Crear tabla en Redshift (si aún no existe)
+            create_table_query = """
+            CREATE TABLE IF NOT EXISTS stock_data (
+                fecha TIMESTAMP,
+                open NUMERIC,
+                high NUMERIC,
+                low NUMERIC,
+                close NUMERIC,
+                volume INT
+            );
+            """
+            cursor.execute(create_table_query)
+            connection.commit()
+
+            # Cargar datos en la tabla
+            for index, row in df.iterrows():
+                insert_query = """
+                INSERT INTO stock_data (fecha, open, high, low, close, volume)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                """
+                cursor.execute(insert_query, (
+                    index,
+                    row['1. open'],
+                    row['2. high'],
+                    row['3. low'],
+                    row['4. close'],
+                    row['5. volume']
+                ))
+            connection.commit()
+
+            # Si hay una alerta, enviar un correo electrónico
+            if mensaje_alerta:
+                send_alert_task = EmailOperator(
+                    task_id="send_alert_task",
+                    to=os.environ.get("AIRFLOW_EMAIL_SENDER"),
+                    subject="Alerta de volumen de acción",
+                    html_content=mensaje_alerta,
+                    dag=dag,
+                )
+
+
+        except (Exception, psycopg2.Error) as error:
+            print(f"Error al conectar o cargar datos en Redshift: {error}")
+        finally:
+            # Cerrar la conexión a la base de datos
+            if connection:
+                cursor.close()
+                connection.close()
     else:
         print(f"Error al hacer la solicitud a la API. Código de estado: {response.status_code}")
         exit()
-
-    # Conectar a la base de datos Redshift
-    try:
-        connection = psycopg2.connect(
-            host=redshift_host,
-            port=redshift_port,
-            dbname=redshift_dbname,
-            user=redshift_user,
-            password=redshift_password
-        )
-        cursor = connection.cursor()
-
-        # Crear tabla en Redshift (si aún no existe)
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS stock_data (
-            fecha TIMESTAMP,
-            open NUMERIC,
-            high NUMERIC,
-            low NUMERIC,
-            close NUMERIC,
-            volume INT
-        );
-        """
-        cursor.execute(create_table_query)
-        connection.commit()
-
-        # Cargar datos en la tabla
-        for index, row in df.iterrows():
-            insert_query = """
-            INSERT INTO stock_data (fecha, open, high, low, close, volume)
-            VALUES (%s, %s, %s, %s, %s, %s);
-            """
-            cursor.execute(insert_query, (
-                index,
-                row['1. open'],
-                row['2. high'],
-                row['3. low'],
-                row['4. close'],
-                row['5. volume']
-            ))
-        connection.commit()
-
-    except (Exception, psycopg2.Error) as error:
-        print(f"Error al conectar o cargar datos en Redshift: {error}")
-    finally:
-        # Cerrar la conexión a la base de datos
-        if connection:
-            cursor.close()
-            connection.close()
 
 # Crear la tarea que ejecuta la función de ingestión de datos
 ingest_data_task = PythonOperator(
@@ -111,3 +140,7 @@ ingest_data_task = PythonOperator(
     python_callable=ingest_data_function,
     dag=dag,
 )
+
+
+
+
